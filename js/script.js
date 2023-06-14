cedd56aecdddc324d164fe9fe22d386bfac90912
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-06-14 13:06:54 +0200

    Struttura

--- js/script.js
+++ js/script.js
@@ -16,43 +16,43 @@
 
 // 1)Chiedere all'utente il numero di km che vuole percorrere
 
-const userDataVoyage = parseInt('Inserisci il numero dei Km che dovrai percorrere');
+// const userDataVoyage = parseInt('Inserisci il numero dei Km che dovrai percorrere');
 
-console.log('userDataVoyage', userDataVoyage , typeof userDataVoyage);
+// console.log('userDataVoyage', userDataVoyage , typeof userDataVoyage);
 
 //2)Chiedere all'utente l'età
-const userDataAge = parseInt('Inserisci la tua età');
+// const userDataAge = parseInt('Inserisci la tua età');
 
-console.log('userDataAge', userDataAge , typeof userDataAge);
+// console.log('userDataAge', userDataAge , typeof userDataAge);
 
 // 3)Calcolare il prezzo totale del viaggio in base alle info dell'utente
 
 //4)Verifica l'età dell'utente
-if( userDataAge < 18){
-    //applico uno sconto del 20%
-    const price = 0.21 * userDataVoyage;
-    const discount = price * 0.20;
-    const priceTotal = price - discount;
+// if( userDataAge < 18){
+//     applico uno sconto del 20%
+//     const price = 0.21 * userDataVoyage;
+//     const discount = price * 0.20;
+//     const priceTotal = price - discount;
 
-    document.querySelector('h4').innerHTML = 'Il Prezzo del biglietto è: €' + priceTotal.toFixed(2);
-}    
+//     document.querySelector('h4').innerHTML = 'Il Prezzo del biglietto è: €' + priceTotal.toFixed(2);
+// }    
 
-else if( userDataAge >= 65){
-    //gli applico uno sconto del 40%
-    const price = 0.21 * userDataVoyage;
-    const discount = price * 0.40;
-    const priceTotal = price - discount;
+// else if( userDataAge >= 65){
+//     gli applico uno sconto del 40%
+//     const price = 0.21 * userDataVoyage;
+//     const discount = price * 0.40;
+//     const priceTotal = price - discount;
 
-    document.querySelector('h4').innerHTML = 'Il Prezzo del biglietto è: €' + priceTotal.toFixed(2);
-}
+//     document.querySelector('h4').innerHTML = 'Il Prezzo del biglietto è: €' + priceTotal.toFixed(2);
+// }
 
-else{
-    //non gli applico nessuno sconto
-    const price = 0.21 * userDataVoyage ;
-    console.log('Il Prezzo per una persona X è' , price);
+// else{
+//     non gli applico nessuno sconto
+//     const price = 0.21 * userDataVoyage ;
+//     console.log('Il Prezzo per una persona X è' , price);
 
-    document.querySelector('h4').innerHTML = 'Il Prezzo del Biglietto è: €' + price.toFixed(2);
-}
+//     document.querySelector('h4').innerHTML = 'Il Prezzo del Biglietto è: €' + price.toFixed(2);
+// }
 
 //Inserisci un allert
 
@@ -65,14 +65,37 @@ else{
 //     alert('Devi inserire i chilometri');
 // }
 
-const myButton = document.getElementById('click-button');
+const clickButton = document.getElementById('click-button');
 
-myButton.addEventListener('click', 
+clickButton.addEventListener('submit', 
     function() {
-        const kmInput = document.querySelector('[name="km"]');
-        const ageInput = document.querySelector('[name="age"]');
+        // 1)Chiedere all'utente il numero di km che vuole percorrere
 
-        h1.innerHTML = 'KM da percorrere: ' + kmInput.value;
-        h1.innerHTML += ' | Età dell utente: ' + ageInput.value;
+        const userDataVoyage = parseInt(document.getElementById(userDataVoyage).value);
+
+        console.log('userDataVoyage', userDataVoyage , typeof userDataVoyage);
+
+        //2)Chiedere all'utente l'età
+        const userDataAge = parseInt(document.getElementById(userDataAge).value);
+
+        console.log('userDataAge', userDataAge , typeof userDataAge);
+
+        if( userDataAge < 18){
+            //applico uno sconto del 20%
+            const price = 0.21 * userDataVoyage;
+            const discount = price * 0.20;
+            const priceTotal = price - discount;
+        
+            document.querySelector('h4').innerHTML = 'Il Prezzo del biglietto è: €' + priceTotal.toFixed(2);
+        }    
+        
+        else if( userDataAge >= 65){
+            //gli applico uno sconto del 40%
+            const price = 0.21 * userDataVoyage;
+            const discount = price * 0.40;
+            const priceTotal = price - discount;
+        
+            document.querySelector('h4').innerHTML = 'Il Prezzo del biglietto è: €' + priceTotal.toFixed(2);
+        }
     }
 );
